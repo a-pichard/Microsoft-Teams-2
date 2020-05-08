c994abfe9cc20352a4a96bdd98f1babb6179635c
Adrien <adrien.pichard@epitech.eu>
2020-05-08 14:20:21 +0200

    ADD: tests

--- common/tests/linked_list/test_linked_list_push_back.c
+++ common/tests/linked_list/test_linked_list_push_back.c
@@ -12,10 +12,53 @@ Test(Test_linked_list, linked_list_push_back)
 {
     ll_t *l = NULL;
     char *data = strdup("toto");
+    char *data2 = strdup("toto2");
 
     ll_push_back(&l, data);
     cr_assert_neq(l, NULL);
     cr_assert_eq(l->data, data);
     cr_assert_eq(l->next, NULL);
+    ll_push_back(&l, data2);
+    cr_assert_eq(l->next->data, data2);
+    cr_assert_eq(l->next->next, NULL);
     free(data);
+    free(data2);
+}
+
+Test(Test_linked_list, linked_list_push_front)
+{
+    ll_t *l = NULL;
+    char *data = strdup("toto");
+    char *data2 = strdup("toto2");
+
+    ll_push_front(&l, data);
+    cr_assert_neq(l, NULL);
+    cr_assert_eq(l->data, data);
+    cr_assert_eq(l->next, NULL);
+    ll_push_front(&l, data2);
+    cr_assert_eq(l->data, data2);
+    cr_assert_eq(l->next->next, NULL);
+    free(data);
+    free(data2);
+}
+
+void destructor(void *data)
+{
+    if (data)
+        free(data);
+}
+
+Test(Test_linked_list, linked_list_erase)
+{
+    ll_t *l = NULL;
+    char *data = strdup("toto");
+    char *data2 = strdup("toto2");
+
+    ll_push_front(&l, data);
+    ll_push_front(&l, data2);
+    ll_erase(&l, l->next, &destructor);
+    cr_assert_eq(l->data, data2);
+    cr_assert_eq(l->next, NULL);
+    free(data);
+    free(data2);
 }

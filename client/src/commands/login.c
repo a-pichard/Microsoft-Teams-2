e082683eddea0e8ec31336fdd887442f825ea879
Adrien Pichard <adrien.pichard@epitech.eu>
2020-05-14 23:55:01 +0200

    FIX: /login and /logout in cli

--- client/src/commands/login.c
+++ client/src/commands/login.c
@@ -10,31 +10,36 @@
 #include "common.h"
 #include "parser.h"
 
-void login(client_t *client, char const * recept)
+static void get_user_infos(char const * const *remainer)
 {
-    char **data = str_to_wordtab(recept, ' ', true);
     char uuid_str[37];
     AND_PARSER(user_p, &UUID_PARSER, &STRING_PARSER, &INT_PARSER);
     parser_t sep_p = SEP_PARSER(' ', &user_p);
     parser_t suround = SUROUNDE_PARSER('"', &sep_p);
+    parser_result_t *r = parse(remainer, &suround);
+    ll_t *user;
+
+    if (r) {
+        user = r->data;
+        uuid_unparse(user->data, uuid_str);
+        client_event_loggedin(uuid_str, user->next->data);
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&suround, r);
+}
+
+void login(client_t *client, char const * recept)
+{
+    char **data = str_to_wordtab(recept, ' ', true);
+    
     parser_result_t *r_status = parse(data, &INT_PARSER);
 
-    if (r_status == NULL) {
-        return; // invalid args
-    }
-    printf("status=%d\n", *(int *)(r_status->data));
-    if (*(int *)(r_status->data) != 200) {
-        //error
-    } else {
-        print_tab(r_status->remainer);
-        parser_result_t *r = parse(r_status->remainer, &suround);
-        if (r) {
-            ll_t *user = r->data;
-            uuid_unparse(user->data, uuid_str);
-            client_event_loggedin(uuid_str, user->next->data);
-        } else {
-            //error
-        }
-    }
-    parser_result_clean(&p, r);
+    if (r_status == NULL)
+        dprintf(1, "Bad reponse.\n");
+    else if (*(int *)(r_status->data) != 200)
+        dprintf(1, "%s\n", *(r_status->remainer));
+    else
+        get_user_infos(r_status->remainer);
+    parser_result_clean(&INT_PARSER, r_status);
 }
--- client/src/commands/logout.c
+++ client/src/commands/logout.c
@@ -6,10 +6,40 @@
 */
 
 #include "client.h"
-#include <logging_client.h>
+#include "logging_client.h"
+#include "common.h"
+#include "parser.h"
+
+static void get_user_infos(char const * const *remainer)
+{
+    char uuid_str[37];
+    AND_PARSER(user_p, &UUID_PARSER, &STRING_PARSER, &INT_PARSER);
+    parser_t sep_p = SEP_PARSER(' ', &user_p);
+    parser_t suround = SUROUNDE_PARSER('"', &sep_p);
+    parser_result_t *r = parse(remainer, &suround);
+    ll_t *user;
+
+    if (r) {
+        user = r->data;
+        uuid_unparse(user->data, uuid_str);
+        client_event_loggedout(uuid_str, user->next->data);
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&suround, r);
+}
 
 void logout(client_t *client, char const * recept)
 {
-    //TODO NEED GOOD DATA (ie user_uuid username)
-    client_event_loggedout(recept, recept);
+    char **data = str_to_wordtab(recept, ' ', true);
+    
+    parser_result_t *r_status = parse(data, &INT_PARSER);
+
+    if (r_status == NULL)
+        dprintf(1, "Bad reponse.\n");
+    else if (*(int *)(r_status->data) != 200)
+        dprintf(1, "%s\n", *(r_status->remainer));
+    else
+        get_user_infos(r_status->remainer);
+    parser_result_clean(&INT_PARSER, r_status);
 }

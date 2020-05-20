f956db866795595343b75dc0da2c5fd3e3d762e7
Killian Provost <killian.provost@epitech.eu>
2020-05-20 14:50:51 +0200

    adding client create command

--- client/src/commands/create.c
+++ client/src/commands/create.c
@@ -7,13 +7,112 @@
 
 #include "client.h"
 #include <logging_client.h>
+#include "common.h"
+#include "parser.h"
+
+static void create_team(char const * const *remainer)
+{
+    char uuid_str[37];
+    AND_PARSER(user_p, &UUID_PARSER, &STRING_PARSER, &STRING_PARSER);
+    parser_result_t *r = parse(remainer, &user_p);
+    ll_t *user;
+
+    if (r) {
+        user = r->data;
+        uuid_unparse(user->data, uuid_str);
+        client_print_team_created(uuid_str, user->next->data, 
+        user->next->next->data);
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&user_p, r);
+}
+
+static void create_channel(char const * const *remainer)
+{
+    char uuid_str[37];
+    AND_PARSER(user_p, &UUID_PARSER, &STRING_PARSER, &STRING_PARSER);
+    parser_result_t *r = parse(remainer, &user_p);
+    ll_t *user;
+
+    if (r) {
+        user = r->data;
+        uuid_unparse(user->data, uuid_str);
+        client_print_channel_created(uuid_str, user->next->data,
+        user->next->next->data);
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&user_p, r);
+}
+
+static void create_thread(char const * const *remainer)
+{
+    char uuid_str[37];
+    char uuid2_str[37];
+    AND_PARSER(user_p, &UUID_PARSER, &UUID_PARSER, &STRING_PARSER,
+    &STRING_PARSER, &STRING_PARSER);
+    parser_result_t *r = parse(remainer, &user_p);
+    ll_t *user;
+
+    if (r) {
+        user = r->data;
+        uuid_unparse(user->data, uuid_str);
+        uuid_unparse(user->next->data, uuid2_str);
+        client_print_thread_created(uuid_str, uuid2_str, 
+        (time_t)user->next->next->data,
+        user->next->next->next->data, user->next->next->next->next->data);
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&user_p, r);
+}
+
+static void create_reply(char const * const *remainer)
+{
+    char uuid_str[37];
+    char uuid_str2[37];
+    AND_PARSER(user_p, &UUID_PARSER, &STRING_PARSER, &STRING_PARSER,
+    &STRING_PARSER);
+    parser_result_t *r = parse(remainer, &user_p);
+    ll_t *user;
+
+    if (r) {
+        user = r->data;
+        uuid_unparse(user->data, uuid_str);
+        uuid_unparse(user->next->data, uuid_str2);
+        client_print_reply_created(uuid_str, uuid_str2, user->next->next->data,
+        user->next->next->next->data);
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&user_p, r);
+}
 
 void create(client_t *client, char const * recept)
 {
-    //switch (client->context)
-    //client_print_team_created(team_id, team_name, team_description);
-    //client_print_channel_created(channel_id, channel_name, channel_description);
-    //client_print_thread_created(thread_id, user_id, thread_timestamp, thread_title, thread_body);
-    //client_print_reply_created(thread_id, user_id, reply_timestamp, reply_body);
-    printf("create = [%s]\n", recept);
+    char **data = str_to_wordtab((char *)recept, ' ', true);
+    parser_result_t *r_status = parse((const char * const *)data, &INT_PARSER);
+
+    if (r_status == NULL)
+        dprintf(1, "Bad reponse.\n");
+    else
+        switch (*(int *)(r_status->data)) {
+            case 200:
+                create_team(r_status->remainer);
+                break;
+            case 201:
+                create_channel(r_status->remainer);
+                break;
+            case 202:
+                create_thread(r_status->remainer);
+                break;
+            case 203:
+                create_reply(r_status->remainer);
+                break;
+            default:
+                dprintf(1, "%s\n", *(r_status->remainer));
+        }
+    destroy_tab(data);
+    parser_result_clean(&INT_PARSER, r_status);
 }

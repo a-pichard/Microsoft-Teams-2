81b07aeb581ed3c19c255dc9111ddef52acb18c9
Killian Provost <killian.provost@epitech.eu>
2020-05-22 12:08:17 +0200

    adding list client command

--- client/src/commands/list.c
+++ client/src/commands/list.c
@@ -7,14 +7,117 @@
 
 #include "client.h"
 #include <logging_client.h>
+#include "common.h"
+#include "parser.h"
+
+static void list_team(char const * const *remainer)
+{
+    char uuid_str[37];
+    AND_PARSER(team_parser, &UUID_PARSER, &STRING_PARSER, &STRING_PARSER);
+    TAB_PARSER(teams_parser, &team_parser);
+    parser_result_t *r = parse(remainer, &teams_parser);
+
+    if (r != NULL) {
+        ll_foreach(r->data, ll_t, l,
+            uuid_unparse(l->data, uuid_str);
+            client_print_teams(uuid_str, l->next->data, l->next->next->data);
+        );
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&teams_parser, r);
+}
+
+static void list_channel(char const * const *remainer)
+{
+    AND_PARSER(channel_parser, &UUID_PARSER, &STRING_PARSER, &STRING_PARSER);
+    TAB_PARSER(channels_parser, &channel_parser);
+    char uuid_str[37];
+    parser_result_t *r = parse(remainer, &channels_parser);
+
+    if (r != NULL) {
+        ll_foreach(r->data, ll_t, l,
+            uuid_unparse(l->data, uuid_str);
+            client_print_channel(uuid_str, l->next->data, l->next->next->data);
+        );
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&channels_parser, r);
+}
+
+static void list_thread(char const * const *remainer)
+{
+    AND_PARSER(thread_parser, &UUID_PARSER, &UUID_PARSER,
+    &STRING_PARSER, &STRING_PARSER, &STRING_PARSER);
+    TAB_PARSER(threads_parser, &thread_parser);
+    char uuid_str[37];
+    char uuid2_str[37];
+    parser_result_t *r = parse(remainer, &threads_parser);
+    ll_t *user;
+
+    if (r != NULL) {
+        ll_foreach(r->data, ll_t, l,
+            uuid_unparse(l->data, uuid_str);
+            uuid_unparse(l->next->data, uuid2_str);
+            client_channel_print_threads(uuid_str, uuid2_str,
+            (time_t)l->next->next->data,
+            l->next->next->next->data,
+            l->next->next->next->next->data);
+        );
+    } else
+        dprintf(1, "Bad reponse.\n");
+    parser_result_clean(&threads_parser, r);
+}
+
+static void list_reply(char const * const *remainer)
+{
+    char uuid_str[37];
+    char uuid_str2[37];
+    AND_PARSER(user_p, &UUID_PARSER, &UUID_PARSER, &STRING_PARSER, 
+    &STRING_PARSER);
+    TAB_PARSER(replys_parser, &user_p);
+    parser_result_t *r = parse(remainer, &replys_parser);
+    ll_t *user;
+
+    if (r != NULL) {
+        ll_foreach(r->data, ll_t, l,
+            uuid_unparse(l->data, uuid_str);
+            uuid_unparse(l->next->data, uuid_str2);
+            client_thread_print_replies(uuid_str, uuid_str2,
+            (time_t)l->next->next->data,
+            l->next->next->next->data);
+        );
+    } else {
+        dprintf(1, "Bad reponse.\n");
+    }
+    parser_result_clean(&replys_parser, r);
+}
 
 void list(client_t *client, char const * recept)
 {
-    //switch (client->context)
-    //client_print_users(user_id, user_name, user_status);
-    //client_print_teams(team_id, team_name, team_description);
-    //client_team_print_channels(channel_id, channel_name, channel_description);
-    //client_channel_print_threads(thread_id, user_id, thread_timestamp, thread_title, thread_body);
-    //client_thread_print_replies(thread_id, user_id, reply_timestamp, reply_body);
-    printf("list = [%s]\n", recept);
+    char **data = str_to_wordtab((char *)recept, ' ', true);
+    parser_result_t *r_status = parse((const char * const *)data, &INT_PARSER);
+
+    if (r_status == NULL)
+        dprintf(1, "Bad reponse.\n");
+    else
+        switch (*(int *)(r_status->data)) {
+            case 200:
+                list_team(r_status->remainer);
+                break;
+            case 201:
+                list_channel(r_status->remainer);
+                break;
+            case 202:
+                list_thread(r_status->remainer);
+                break;
+            case 203:
+                list_reply(r_status->remainer);
+                break;
+            default:
+                !*r_status->remainer ? 0 : dprintf(1, "%s\n", *(r_status->remainer));
+        }
+    destroy_tab(data);
+    parser_result_clean(&INT_PARSER, r_status);
 }
